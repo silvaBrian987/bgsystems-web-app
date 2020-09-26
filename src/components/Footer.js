e5ee2f0b833236a64a7c9310854600e665309f7e
Brian Gabriel Silva <silva.brian.987@gmail.com>
2020-09-26 20:34:10 -0300

    Se sube el primer código

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="jumbotron text-center mb-0">
+            <p>Footer</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import logo from '../logo.svg';
+
+import Main from '../components/Main';
+import NoMatch from './NoMatch';
+
+function Header() {
+    return (
+        <Router>
+            <div className="jumbotron text-center mb-0">
+                <h1>BGSystems's homepage</h1>
+                <p>Welcome! Welcome to my page.</p>
+            </div>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+                <Link className="nav-link" to="/"><img src={logo} alt="Logo" style={{ width: 40 + 'px' }} /></Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">CV</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/projects">Projects</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <Switch>
+                <Route path="/about">
+                    {/* <About /> */}
+                </Route>
+                <Route path="/users">
+                    {/* <Users /> */}
+                </Route>
+                <Route exact path="/">
+                    <Main />
+                </Route>
+                <Route path="*">
+                    <NoMatch />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+export default Header;
